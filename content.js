a49c642886f36b72338f6d829e0e48c3dac33b00
Sam Ahmed <samuelkahmed@gmail.com>
2024-06-17 16:07:35 +0200

    1.1 colour key
    Added key to the map with guide for what numbers the colours represent.

--- content.js
+++ content.js
@@ -3,33 +3,3 @@ const scriptEl = document.createElement('script');
 scriptEl.setAttribute('type', 'text/javascript');
 scriptEl.setAttribute('src', chrome.runtime.getURL('inject.js'));
 bodyEl.appendChild(scriptEl);
-
-/*
-const mapEl = document.getElementsByClassName('svgMap-map-controls-wrapper');
-const keyWrapper = document.createElement("div");
-keyWrapper.classList.add("map-colour-key-wrapper");
-keyWrapper.style.cursor = 'pointer';
-keyWrapper.style.height = '30px';
-keyWrapper.style.position = 'relative';
-keyWrapper.style.background = "#2c3440";
-const colourKeyA = document.createElement("div");
-const colourKeyB = document.createElement("div");
-const colourKeyC = document.createElement("div");
-const colourKeyD = document.createElement("div");
-const colourKeyE = document.createElement("div");
-const colourKeys = [colourKeyA, colourKeyB, colourKeyC, colourKeyD, colourKeyE];
-colourKeys.forEach(value => {
-    value.style.height = '20px';
-    value.style.width = '20px';
-    value.style.position = 'relative';
-    keyWrapper.appendChild(value);
-})
-colourKeyA.style.background = 'hsl(143, 100%, 23%)';
-colourKeyB.style.background = 'hsl(143, 100%, 61%)';
-colourKeyC.style.background = 'hsl(143, 100%, 100%)';
-colourKeyD.style.background = 'hsl(30, 100%, 75%)';
-colourKeyE.style.background = 'hsl(30, 100%, 51%)';
-[...mapEl].forEach(value => {
-    value.appendChild(keyWrapper);
-})
-    */
--- inject.js
+++ inject.js
@@ -45,3 +45,48 @@ const medianCount = uniqueCounts[Math.floor((uniqueCounts.length - 1) / 2)];
         path.style.fill = fillReplace;
     })
 });
+
+const mapEl = document.getElementsByClassName('svgMap-map-controls-wrapper');
+const keyWrapper = document.createElement("div");
+keyWrapper.classList.add("map-colour-key-wrapper");
+//keyWrapper.style.cursor = 'default';
+keyWrapper.style.height = '30px';
+keyWrapper.style.position = 'relative';
+keyWrapper.style.background = "#2c3440";
+keyWrapper.style.display = 'flex';
+const colourKeyA = document.createElement("div");
+const colourKeyB = document.createElement("div");
+const colourKeyC = document.createElement("div");
+const colourKeyD = document.createElement("div");
+const colourKeyE = document.createElement("div");
+const colourKeys = [colourKeyA, colourKeyB, colourKeyC, colourKeyD, colourKeyE];
+const keyNumbers = [1, Math.round((1 + medianCount) / 2), medianCount, Math.round(Math.exp((Math.log(medianCount) + Math.log(maxWatchCount)) / 2)), maxWatchCount]
+colourKeys.forEach((colkey, index) => {
+    colkey.style.height = '20px';
+    colkey.style.width = '20px';
+    colkey.style.position = 'relative';
+    colkey.style.margin = '5px';
+    colkey.style.display = 'flex';
+    colkey.style.justifyContent = 'center';
+    colkey.style.alignItems = 'center';
+    //colkey.style.fontWeight = 'bold';
+    //colkey.style.fontFamily = 'TiemposTextWeb-Semibold, Georgia, serif';
+    let innerNum = keyNumbers[index];
+    colkey.innerText = innerNum;
+    colkey.classList.add('col-key');
+    keyWrapper.appendChild(colkey);
+})
+colourKeyA.style.background = 'hsl(143, 100%, 23%)';
+colourKeyB.style.background = 'hsl(143, 100%, 61%)';
+colourKeyC.style.background = 'hsl(143, 100%, 100%)';
+colourKeyD.style.background = 'hsl(30, 100%, 75%)';
+colourKeyE.style.background = 'hsl(30, 100%, 51%)';
+colourKeyA.style.color = 'hsl(143, 100%, 61%)';
+colourKeyB.style.color = 'hsl(143, 100%, 23%)';
+colourKeyC.style.color = '#2c3440';
+colourKeyD.style.color = 'hsl(30, 100%, 45%)';
+colourKeyE.style.color = 'hsl(30, 100%, 90%)';
+
+[...mapEl].forEach(value => {
+    value.appendChild(keyWrapper);
+})
